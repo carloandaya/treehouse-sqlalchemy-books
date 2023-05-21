7f234e0f74c0af1377c88dd2d15ff2ffa23041f3
Carlo Andaya <2507017+carloandaya@users.noreply.github.com>
2023-05-21 01:14:12 -0700

    Add a book.

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 from models import Base, Book, session, engine
-from datetime import datetime
+import datetime
 import csv
 
 
 def clean_date(publish_date: str) -> datetime.date:
-    clean_date = datetime.strptime(publish_date, "%B %d, %Y").date()
-    return clean_date
+    try:
+        clean_date = datetime.datetime.strptime(publish_date, "%B %d, %Y").date()
+    except ValueError:
+        input("*** DATE ERROR ***")
+        return
+    else:
+        return clean_date
 
 
 def clean_price(price_str):
-    price_float = float(price_str)
-    return int(price_float * 100)
-    
+    try:
+        price_float = float(price_str)
+    except ValueError:
+        input("*** PRICE ERROR ***")
+        return
+    else:
+        return int(price_float * 100)
+
 
 def add_csv():
     with open("suggested_books.csv") as csvfile:
         data = csv.reader(csvfile)
-        for row in data: 
-            book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
+        for row in data:
+            book_in_db = session.query(Book).filter(Book.title == row[0]).one_or_none()
             if book_in_db == None:
-                book = Book(title=row[0], author=row[1], 
-                            publish_date=clean_date(row[2]),
-                            price=clean_price(row[3]))
+                book = Book(
+                    title=row[0],
+                    author=row[1],
+                    publish_date=clean_date(row[2]),
+                    price=clean_price(row[3]),
+                )
                 session.add(book)
+
         session.commit()
 
 
@@ -54,8 +68,27 @@ def app():
 
         match choice:
             case 1:
-                print("add book")
-                add_csv()
+                title = input("Title: ")
+                author = input("Author: ")
+                date_error = True
+                while date_error:
+                    publish_date = input("Published Date: ")
+                    publish_date = clean_date(publish_date)
+                    if type(publish_date) == datetime.date:
+                        date_error = False
+                price_error = True
+                while price_error:
+                    price = input("Price: ")
+                    price = clean_price(price)
+                    if type(price) == int:
+                        price_error = False
+
+                new_book = Book(
+                    title=title, author=author, publish_date=publish_date, price=price
+                )
+                session.add(new_book)
+                session.commit()
+                print("Book added!")
             case 2:
                 print("view all books")
             case 5:
@@ -67,5 +100,5 @@ def app():
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
-
+    add_csv()
     app()
